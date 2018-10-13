5166c1b2537cd7ff2276c1cd83f0be6e06c2367a
Victor Rosa <victor@m2mtelemetria.com.br>
2018-10-13 19:47:01 -0300

    Update Method and Intent implemented

--- app/src/main/java/com/platiplus/agenda/DAO/AlunoDAO.java
+++ app/src/main/java/com/platiplus/agenda/DAO/AlunoDAO.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.NonNull;
 
 import com.platiplus.agenda.Model.Aluno;
 
@@ -31,6 +32,13 @@ public class AlunoDAO extends SQLiteOpenHelper {
 
     public void insere(Aluno aluno) {
         SQLiteDatabase db = getWritableDatabase();
+        ContentValues dados = pegaDadosAluno(aluno);
+
+        db.insert("Alunos", null, dados);
+        }
+
+    @NonNull
+    private ContentValues pegaDadosAluno(Aluno aluno) {
         ContentValues dados = new ContentValues();
 
         dados.put("nome", aluno.getNome());
@@ -38,9 +46,8 @@ public class AlunoDAO extends SQLiteOpenHelper {
         dados.put("telefone", aluno.getTelefone());
         dados.put("site", aluno.getSite());
         dados.put("nota", aluno.getNota());
-
-        db.insert("Alunos", null, dados);
-        }
+        return dados;
+    }
 
     public List<Aluno> buscaAlunos() {
         String sql = "SELECT * FROM Alunos";
@@ -72,5 +79,13 @@ public class AlunoDAO extends SQLiteOpenHelper {
         SQLiteDatabase db = getWritableDatabase();
         String[] params = {aluno.getId().toString()};
         db.delete("alunos", "id = ?", params);
+    }
+
+    public void altera(Aluno aluno) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues dados = pegaDadosAluno(aluno);
+        String[] params = {aluno.getId().toString()};
+
+        db.update("Alunos", dados, "id = ?", params );
     }
 }
